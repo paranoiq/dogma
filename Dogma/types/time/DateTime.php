d440c38da51c3dda3f7b0f8c34f4b41e38f41581
Vlasta Neubauer <neubauer@vspdata.cz>
2012-05-22 10:26:03 +0200

    DateTime: removed support for change notification

--- Dogma/types/time/DateTime.php
+++ Dogma/types/time/DateTime.php
@@ -8,9 +8,6 @@ namespace Dogma;
  */
 class DateTime extends \DateTime {
     
-    public $onChange = array();
-
-
     /**
      * @param string
      * @param \DateTimeZone
@@ -43,84 +40,6 @@ class DateTime extends \DateTime {
         }
         
         return $date;
-    }
-
-
-    /**
-     * @param \DateInterval
-     * @return self
-     */
-    public function add(/*\DateInterval*/ $interval) {
-        return parent::add($interval);
-    }
-
-
-    /**
-     * @param \DateInterval
-     * @return self
-     */
-    public function sub(/*\DateInterval*/ $interval) {
-        return parent::sub($interval);
-    }
-
-
-    /**
-     * @param string
-     * @return self
-     */
-    public function modify($modify) {
-        return parent::modify($modify);
-    }
-
-
-    /**
-     * @param int
-     * @param int
-     * @param int
-     * @return self
-     */
-    public function setDate($year, $month, $day) {
-        return parent::setDate($year, $month, $day);
-    }
-
-
-    /**
-     * @param int
-     * @param int
-     * @param int
-     * @return self
-     */
-    public function setISODate($year, $week, $day = 1) {
-        return parent::setISODate($year, $week, $day);
-    }
-
-
-    /**
-     * @param int
-     * @param int
-     * @param int
-     * @return self
-     */
-    public function setTime($hour, $minute, $second = 0) {
-        return parent::setTime($hour, $minute, $second);
-    }
-
-
-    /**
-     * @param int
-     * @return self
-     */
-    public function setTimestamp($timestamp) {
-        return parent::setTimestamp($timestamp);
-    }
-
-
-    /**
-     * @param DateTimeZone
-     * @return self
-     */
-    public function setTimezone(/*\DateTimeZone*/ $timezone) {
-        return parent::setTimezone($timezone);
     }
     
     
