6b78736ba5cafa2bf2103a9a4d96dbd78d77b2e2
Vlasta Neubauer <neubauer@vspdata.cz>
2012-07-06 12:34:29 +0200

    Regexp: uses Nette\Strings; bugfixes

--- Dogma/common/types/Regexp.php
+++ Dogma/common/types/Regexp.php
@@ -2,20 +2,22 @@
 
 namespace Dogma;
 
-use Nette\Diagnostics\Debugger;
-use Nette\Callback;
-use Nette\Utils\RegexpException;
-
-//class RegexpException extends \Nette\RegexpException {}
+use Nette\Utils\Strings;
 
 
 /**
  * Regular expression object
- * @see http://phpfashion.com/zradne-regularni-vyrazy-v-php
- * @author David Grudl
- * @author Vlasta Neubauer
  */
-class Regexp extends \Nette\Object {
+class Regexp extends \Dogma\Object {
+    
+    const
+        DOLLAR_MATCH_END_ONLY = 'D',
+        CASE_INSENSITIVE = 'i',
+        MULTILINE = 'm',
+        DOT_MATCH_EOL = 's',
+        UNICODE = 'u',
+        UNGREEDY = 'U',    
+        IGNORE_WHITE_SPACE = 'x';
     
     
     /** @var string */
@@ -26,11 +28,7 @@ class Regexp extends \Nette\Object {
      * @param string
      */    
     public function __construct($pattern) {
-        //if ($pattern instanceof \MongoRegex) {
-        //    $this->pattern = '/' . str_replace('/', '\/', $pattern->regex) . '/' . $pattern->flags;
-        //} else {
-            $this->pattern = $pattern;
-        //}
+        $this->pattern = $pattern;
     }
 
 
@@ -49,10 +47,7 @@ class Regexp extends \Nette\Object {
      * @return array
      */
     public function split($subject, $flags = 0) {
-        Debugger::tryError();
-        $res = preg_split($this->pattern, $subject, -1, $flags | PREG_SPLIT_DELIM_CAPTURE);
-        self::catchPregError($this->pattern);
-        return $res;
+        return Strings::split($subject, $this->pattern, $flags);
     }
     
     
@@ -64,12 +59,7 @@ class Regexp extends \Nette\Object {
      * @return mixed
      */
     public function match($subject, $flags = 0, $offset = 0) {
-        Debugger::tryError();
-        $res = preg_match($this->pattern, $subject, $m, $flags, $offset);
-        self::catchPregError($this->pattern);
-        if ($res) {
-            return $m;
-        }
+        return Strings::match($subject, $this->pattern, $flags, $offset);
     }
     
     
@@ -81,10 +71,7 @@ class Regexp extends \Nette\Object {
      * @return array
      */
     public function matchAll($subject, $flags = 0, $offset = 0) {
-        Debugger::tryError();
-        $res = preg_match_all($this->pattern, $subject, $m, ($flags & PREG_PATTERN_ORDER) ? $flags : ($flags | PREG_SET_ORDER), $offset);
-        self::catchPregError($this->pattern);
-        return $m;
+        return Strings::matchAll($subject, $this->pattern, $flags, $offset);
     }
     
     
@@ -96,89 +83,7 @@ class Regexp extends \Nette\Object {
      * @return string
      */
     public function replace($subject, $replacement = NULL, $limit = -1) {
-        Debugger::tryError();
-        if (is_object($replacement) || is_array($replacement)) {
-            if ($replacement instanceof Callback) {
-                $replacement = $replacement->getNative();
-            }
-            if (!is_callable($replacement, FALSE, $textual)) {
-                Debugger::catchError($foo);
-                throw new \Nette\InvalidStateException("Regexp: Callback '$textual' is not callable.");
-            }
-            $res = preg_replace_callback($this->pattern, $replacement, $subject, $limit);
-            
-            if (Debugger::catchError($e)) { // compile error
-                $trace = $e->getTrace();
-                if (isset($trace[2]['class']) && $trace[2]['class'] === __CLASS__) {
-                    throw new RegexpException("Regexp: " . $e->getMessage() . " in pattern: $this->pattern");
-                }
-            }
-            
-        } else {
-            $res = preg_replace($this->pattern, $replacement, $subject, $limit);
-        }
-        self::catchPregError($this->pattern);
-        return $res;
-    }
-    
-    
-    /**
-     * Perform a regular expression search and replace.
-     * @param  string
-     * @param  string|array
-     * @param  string|callback
-     * @param  int
-     * @return string
-     */
-    public static function multiReplace($subject, $pattern, $replacement = NULL, $limit = -1) {
-        Debugger::tryError();
-        if (is_object($replacement) || is_array($replacement)) {
-            if ($replacement instanceof Callback) {
-                $replacement = $replacement->getNative();
-            }
-            if (!is_callable($replacement, FALSE, $textual)) {
-                Debugger::catchError($foo);
-                throw new \Nette\InvalidStateException("Regexp: Callback '$textual' is not callable.");
-            }
-            $res = preg_replace_callback($pattern, $replacement, $subject, $limit);
-            
-            if (Debugger::catchError($e)) { // compile error
-                $trace = $e->getTrace();
-                if (isset($trace[2]['class']) && $trace[2]['class'] === __CLASS__) {
-                    throw new RegexpException("Regexp: " . $e->getMessage() . " in pattern: $pattern");
-                }
-            }
-            
-        } elseif (is_array($pattern)) {
-            $res = preg_replace(array_keys($pattern), array_values($pattern), $subject, $limit);
-            
-        } else {
-            $res = preg_replace($pattern, $replacement, $subject, $limit);
-        }
-        self::catchPregError($pattern);
-        return $res;
-    }
-
-
-    /**
-     * @internal
-     * @param $pattern
-     */
-    public static function catchPregError($pattern) {
-        if (Debugger::catchError($e)) { // compile error
-            throw new RegexpException("Regexp: " . $e->getMessage() . " in pattern: $pattern");
-            
-        } elseif (preg_last_error()) { // run-time error
-            static $messages = array(
-                PREG_INTERNAL_ERROR => 'Internal error',
-                PREG_BACKTRACK_LIMIT_ERROR => 'Backtrack limit was exhausted',
-                PREG_RECURSION_LIMIT_ERROR => 'Recursion limit was exhausted',
-                PREG_BAD_UTF8_ERROR => 'Malformed UTF-8 data',
-                5 => 'Offset didn\'t correspond to the begin of a valid UTF-8 code point', // PREG_BAD_UTF8_OFFSET_ERROR
-            );
-            $code = preg_last_error();
-            throw new RegexpException((isset($messages[$code]) ? $messages[$code] : 'Unknown error') . " (pattern: $pattern)", $code);
-        }
+        return Strings::replace($subject, $this->pattern, $replacement, $limit);
     }
     
 }
