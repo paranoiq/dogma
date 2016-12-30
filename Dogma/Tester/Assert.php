5de2c038914ff8eb45db91d9a9c1213a3ae0d590
Vlasta Neubauer <paranoiq@centrum.cz>
2016-12-30 02:03:08 +0100

    Updated dependencies; Added PHPStan

--- Dogma/Tester/Assert.php
+++ Dogma/Tester/Assert.php
@@ -8,54 +8,54 @@ namespace Dogma\Tester;
 class Assert extends \Tester\Assert
 {
 
-    public static function same($actual, $expected)
+    public static function same($actual, $expected, $description = null)
     {
-        parent::same($expected, $actual);
+        parent::same($expected, $actual, $description);
     }
 
-    public static function notSame($actual, $expected)
+    public static function notSame($actual, $expected, $description = null)
     {
-        parent::notSame($expected, $actual);
+        parent::notSame($expected, $actual, $description);
     }
 
-    public static function equal($actual, $expected)
+    public static function equal($actual, $expected, $description = null)
     {
-        parent::equal($expected, $actual);
+        parent::equal($expected, $actual, $description);
     }
 
-    public static function notEqual($actual, $expected)
+    public static function notEqual($actual, $expected, $description = null)
     {
-        parent::notEqual($expected, $actual);
+        parent::notEqual($expected, $actual, $description);
     }
 
-    public static function contains($haystack, $needle)
+    public static function contains($haystack, $needle, $description = null)
     {
-        parent::contains($needle, $haystack);
+        parent::contains($needle, $haystack, $description);
     }
 
-    public static function notContains($haystack, $needle)
+    public static function notContains($haystack, $needle, $description = null)
     {
-        parent::notContains($needle, $haystack);
+        parent::notContains($needle, $haystack, $description);
     }
 
-    public static function count($actualValue, $expectedCount)
+    public static function count($actualValue, $expectedCount, $description = null)
     {
-        parent::count($expectedCount, $actualValue);
+        parent::count($expectedCount, $actualValue, $description);
     }
 
-    public static function type($actualValue, $expectedType)
+    public static function type($actualValue, $expectedType, $description = null)
     {
-        parent::type($expectedType, $actualValue);
+        parent::type($expectedType, $actualValue, $description);
     }
 
-    public static function match($actualValue, $mask)
+    public static function match($actualValue, $mask, $description = null)
     {
-        parent::match($mask, $actualValue);
+        parent::match($mask, $actualValue, $description);
     }
 
-    public static function matchFile($actualValue, $file)
+    public static function matchFile($actualValue, $file, $description = null)
     {
-        parent::matchFile($file, $actualValue);
+        parent::matchFile($file, $actualValue, $description);
     }
 
     public static function fail($message, $actual = null, $expected = null)
