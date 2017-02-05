e44e1f89b1e49f02e0600dc5d416482ac3f153e6
Vlasta Neubauer <paranoiq@centrum.cz>
2017-02-05 10:27:59 +0100

    Add PowersOfTwo constants

---
+++ Dogma/common/consts/PowersOfTwo.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of the Dogma library (https://github.com/paranoiq/dogma)
+ *
+ * Copyright (c) 2012 Vlasta Neubauer (@paranoiq)
+ *
+ * For the full copyright and license information read the file 'license.md', distributed with this source code
+ */
+
+namespace Dogma;
+
+final class PowersOfTwo
+{
+    use \Dogma\StaticClassMixin;
+
+    const _1 = 1; // 0
+    const _2 = 2;
+    const _4 = 4;
+    const _8 = 8;
+    const _16 = 16;
+    const _32 = 32;
+    const _64 = 64;
+    const _128 = 128;
+    const _256 = 256; // 8
+    const _512 = 512;
+    const _1K = 1024;
+    const _2K = 2048;
+    const _4K = 4096;
+    const _8K = 8192;
+    const _16K = 16384;
+    const _32K = 32768;
+    const _64K = 65536; // 16
+    const _128K = 131072;
+    const _256K = 262144;
+    const _512K = 524288;
+    const _1M = 1048576;
+    const _2M = 2097152;
+    const _4M = 4194304;
+    const _8M = 8388608;
+    const _16M = 16777216; // 24
+    const _32M = 33554432;
+    const _64M = 67108864;
+    const _128M = 134217728;
+    const _256M = 268435456;
+    const _512M = 536870912;
+    const _1G = 1073741824;
+    const _2G = 2147483648;
+    const _4G = 4294967296; // 32
+    const _8G = 8589934592;
+    const _16G = 17179869184;
+    const _32G = 34359738368;
+    const _64G = 68719476736;
+    const _128G = 137438953472;
+    const _256G = 274877906944;
+    const _512G = 549755813888;
+    const _1T = 1099511627776; // 40
+    const _2T = 2199023255552;
+    const _4T = 4398046511104;
+    const _8T = 8796093022208;
+    const _16T = 17592186044416;
+    const _32T = 35184372088832;
+    const _64T = 70368744177664;
+    const _128T = 140737488355328;
+    const _256T = 281474976710656; // 48
+    const _512T = 562949953421312;
+    const _1P = 1125899906842624;
+    const _2P = 2251799813685248;
+    const _4P = 4503599627370496;
+    const _8P = 9007199254740992;
+    const _16P = 18014398509481984;
+    const _32P = 36028797018963968;
+    const _64P = 72057594037927936; // 56
+    const _128P = 144115188075855872;
+    const _256P = 288230376151711744;
+    const _512P = 576460752303423488;
+    const _1E = 1152921504606846976;
+    const _2E = 2305843009213693952;
+    const _4E = 4611686018427387904; // 63
+
+}
