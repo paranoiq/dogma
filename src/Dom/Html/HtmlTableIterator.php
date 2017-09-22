c62180ce3898cc067e829306e15250743be44684
Vlasta Neubauer <paranoiq@centrum.cz>
2017-09-22 12:18:20 +0200

    Move HtmlTableIterator to Dom

--- src/Dom/Html/HtmlTableIterator.php
+++ src/Dom/Html/HtmlTableIterator.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information read the file 'license.md', distributed with this source code
  */
 
-namespace Dogma\Datasheet;
+namespace Dogma\Dom\Html;
 
 use Dogma\Dom\Element;
 
