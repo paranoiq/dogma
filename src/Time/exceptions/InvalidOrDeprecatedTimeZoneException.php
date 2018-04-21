10260193c804398df9249d5075d9a631a4b5f02e
Vlasta Neubauer <paranoiq@centrum.cz>
2018-04-21 12:46:38 +0200

    Add TimeZone enum/factory

---
+++ src/Time/exceptions/InvalidOrDeprecatedTimeZoneException.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+/**
+ * This file is part of the Dogma library (https://github.com/paranoiq/dogma)
+ *
+ * Copyright (c) 2012 Vlasta Neubauer (@paranoiq)
+ *
+ * For the full copyright and license information read the file 'license.md', distributed with this source code
+ */
+
+namespace Dogma\Time;
+
+use Dogma\Exception;
+
+class InvalidOrDeprecatedTimeZoneException extends Exception
+{
+
+    public function __construct(string $name, ?\Throwable $previous = null)
+    {
+        $message = sprintf(
+            'Time zone name "%s" is not valid or is deprecated. See https://secure.php.net/manual/en/timezones.others.php for deprecated time zones info.',
+            $name
+        );
+
+        parent::__construct($message, $previous);
+    }
+
+}
