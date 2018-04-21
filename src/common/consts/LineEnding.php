31f181105d836c7d587f4832cf2f38f57aa454fa
Vlasta Neubauer <paranoiq@centrum.cz>
2018-04-21 11:35:57 +0200

    Add line ending consts

---
+++ src/common/consts/LineEnding.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+/**
+ * This file is part of the Dogma library (https://github.com/paranoiq/dogma)
+ *
+ * Copyright (c) 2012 Vlasta Neubauer (@paranoiq)
+ *
+ * For the full copyright and license information read the file 'license.md', distributed with this source code
+ */
+
+// spell-check-ignore: CRLF LF
+
+namespace Dogma;
+
+class LineEnding
+{
+    use StaticClassMixin;
+
+    public const LF = "\n";
+    public const CRLF = "\r\n";
+    public const CR = "\r";
+
+}
