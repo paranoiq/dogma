82ce20477ee9c4ba89b22dedd5af9defe39028e0
Vlasta Neubauer <cz.paranoiq@gmail.com>
2019-07-15 13:09:38 +0200

    Support for 48bit integer bounds

--- src/common/consts/BitSize.php
+++ src/common/consts/BitSize.php
@@ -21,6 +21,7 @@ class BitSize
     public const BITS_16 = 16;
     public const BITS_24 = 24;
     public const BITS_32 = 32;
+    public const BITS_48 = 48;
     public const BITS_64 = 64;
 
     public const DEFAULT_INT_SIZE = PHP_INT_SIZE * 8;
@@ -36,6 +37,7 @@ class BitSize
             self::BITS_16,
             self::BITS_24,
             self::BITS_32,
+            self::BITS_48,
             self::BITS_64,
         ];
     }
@@ -85,9 +87,10 @@ class BitSize
         $bounds = [
             Sign::UNSIGNED => [
                 self::BITS_8 => [0, 255],
-                self::BITS_16 => [0, 65536],
-                self::BITS_24 => [0, 16777216],
-                self::BITS_32 => [0, 4294967296],
+                self::BITS_16 => [0, 65535],
+                self::BITS_24 => [0, 16777215],
+                self::BITS_32 => [0, 4294967295],
+                self::BITS_48 => [0, 281474976710655],
                 self::BITS_64 => [0, PHP_INT_MAX], // this is actually 63 bits, since PHP int is always signed
             ],
             Sign::SIGNED => [
@@ -95,6 +98,7 @@ class BitSize
                 self::BITS_16 => [-32768, 32767],
                 self::BITS_24 => [-8388608, 8388607],
                 self::BITS_32 => [-2147483648, 2147483647],
+                self::BITS_48 => [-140737488355328, 140737488355327],
                 self::BITS_64 => [PHP_INT_MIN, PHP_INT_MAX],
             ],
         ];
